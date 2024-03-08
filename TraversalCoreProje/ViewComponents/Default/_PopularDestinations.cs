65a154841c7589aa55214d59d36c9e09d0715eeb
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-03-08 21:47:35 +0300

    added destinations statistics

---
+++ TraversalCoreProje/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _PopularDestinations : ViewComponent
+    {
+        DestinationManager destinationManager = new(new EfDestinationDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.GetList();
+            return View(values);
+        }
+    }
+}

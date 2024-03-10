d53188bc7c0a13045e083c2bf707cfc28d05da9f
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-03-10 05:28:15 +0300

    update layout

---
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new(new EfDestinationDal());
+
+        public IActionResult Index()
+        {
+            var values = destinationManager.GetList();
+            return View(values);
+        }
+    }
+}

ab228cdf9ca686eccf200713da2a719b08a377b8
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-05-03 00:43:34 +0300

    Commenting has been completed

--- BusinessLayer/Concrete/CommentManager.cs
+++ BusinessLayer/Concrete/CommentManager.cs
@@ -30,7 +30,7 @@ namespace BusinessLayer.Concrete
 
         public void TAdd(Comment t)
         {
-            throw new NotImplementedException();
+            _commentDal.Insert(t);
         }
 
         public void TDelete(Comment t)
---
+++ TraversalCoreProje/Controllers/CommentController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentDal());
+
+        [HttpGet]
+        public PartialViewResult AddComment(int id)
+        {
+            ViewBag.i = id;
+            return PartialView();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddComment(Comment comment)
+        {
+            comment.CommentDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            comment.CommentState = true;
+            commentManager.TAdd(comment);
+            return RedirectToAction("Index", "Destination");
+        }
+    }
+}

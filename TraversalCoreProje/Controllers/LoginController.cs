061a5b6dc2b46cc6b774a5c6757c99ae915e2c56
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-05-03 02:18:02 +0300

    Login and Register Pages created

---
+++ TraversalCoreProje/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    //kimlik doğrulama gerekmeksizin erişilmesine izin verir.
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult SignIn()
+        //{
+        //    return View();
+        //}
+    }
+}

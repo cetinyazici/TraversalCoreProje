842691661c9afe9018075149376ab8be5e95dc55
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-05-03 18:53:05 +0300

    Listing of Data was made on the Member Page

---
+++ TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
+        public IActionResult Index()
+        {
+            var values = destinationManager.GetList();
+            return View(values);
+        }
+    }
+}

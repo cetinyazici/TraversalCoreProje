d1febaf26b6786c4ed049ba33721763fe4468502
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-05-06 00:36:13 +0300

    Admin Areas created

---
+++ TraversalCoreProje/Areas/Admin/Controllers/DestinationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Member/[controller]/[action]")]
+    public class DestinationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

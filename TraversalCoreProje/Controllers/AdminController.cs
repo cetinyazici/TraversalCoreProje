776d5cf7c5712cec3a9e9ef35248ee34d2aeecc5
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-05-07 00:29:35 +0300

    Admin Layout PartialView created

--- TraversalCoreProje/Controllers/AdminController.cs
+++ TraversalCoreProje/Controllers/AdminController.cs
@@ -8,5 +8,30 @@ namespace TraversalCoreProje.Controllers
         {
             return PartialView();
         }
+
+        public PartialViewResult PartialAppBrandDemo()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
     }
 }

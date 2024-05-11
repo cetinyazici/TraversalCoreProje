e4d475e806bff4a3a83504782cb7ad14e6b51e51
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-05-11 15:57:28 +0300

    Ajax Transactions 1

---
+++ TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(cityModels);
+            return Json(jsonCity);
+        }
+
+        public static List<CityModel> cityModels = new List<CityModel>
+        {
+            new CityModel()
+            {
+                CityId = 1,
+                CityName = "Test",
+                CityCountry="Test",
+            },
+            new CityModel()
+            {
+                CityId= 2,
+                CityName= "Test",
+                CityCountry="Test",
+            },
+            new CityModel()
+            {
+                CityId =3,
+                CityName= "Test",
+                CityCountry="Test",
+            }
+        };
+
+    }
+}

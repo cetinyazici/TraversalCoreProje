55b6a400afb29ed4b2a398b3d713fd0aad255ea1
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-05-11 12:47:35 +0300

    Guide List operations have been completed

--- TraversalCoreProje/Areas/Admin/Controllers/GuideController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/GuideController.cs
@@ -7,49 +7,60 @@ using Microsoft.AspNetCore.Mvc;
 namespace TraversalCoreProje.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Route("Admin/Guide")]
     public class GuideController : Controller
     {
         private readonly IGuideService _guideService;
-
         public GuideController(IGuideService guideService)
         {
             _guideService = guideService;
         }
 
+        [Route("")]
+        [Route("Index")]
         public IActionResult Index()
         {
             var values = _guideService.GetList();
             return View(values);
         }
 
+        [Route("AddGuide")]
         [HttpGet]
         public IActionResult AddGuide()
         {
             return View();
         }
 
+        [Route("AddGuide")]
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public IActionResult AddGuide(Guide guide)
         {
-            GuideValidator validationRules = new();
-            ValidationResult validationResult = validationRules.Validate(guide);
-            if (validationResult.IsValid)
+            GuideValidator validationRules = new GuideValidator();
+            ValidationResult result = validationRules.Validate(guide);
+            if (result.IsValid)
             {
                 _guideService.TAdd(guide);
                 return RedirectToAction("Index");
             }
             else
             {
-                foreach (var item in validationResult.Errors)
+                foreach (var item in result.Errors)
                 {
                     ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
                 }
                 return View();
             }
-
         }
 
+        [Route("DeleteGuide/{id}")]
+        public IActionResult DeleteGuide(int id)
+        {
+            var values = _guideService.TGetById(id);
+            _guideService.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [Route("EditGuide/{id}")]
         [HttpGet]
         public IActionResult EditGuide(int id)
         {
@@ -57,23 +68,26 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
             return View(values);
         }
 
+        [Route("EditGuide/{id}")]
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public IActionResult EditGuide(Guide guide)
         {
             _guideService.TUpdate(guide);
             return RedirectToAction("Index");
         }
 
+        [Route("ChangeToTrue/{id}")]
         public IActionResult ChangeToTrue(int id)
         {
-            return RedirectToAction("Index");
+            _guideService.TChangeToTrueByGuide(id);
+            return RedirectToAction("Index", "Guide", new { area = "Admin" });
         }
 
-        public IActionResult ChangeToFalse()
+        [Route("ChangeToFalse/{id}")]
+        public IActionResult ChangeToFalse(int id)
         {
-            return RedirectToAction("Index");
+            _guideService.TChangeToFalseByGuide(id);
+            return RedirectToAction("Index", "Guide", new { area = "Admin" });
         }
-
     }
 }

950050e8770ccd4ef58effbfccced3de2f2d94ed
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-05-20 02:32:35 +0300

    PostgreSQL Database connection has been made.

---
+++ Traversal/SignalRApi/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SignalRApi.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+
+}
--- Traversal/SignalRApi/Program.cs
+++ Traversal/SignalRApi/Program.cs
@@ -1,7 +1,17 @@
+using FluentAssertions.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SignalRApi.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<Context>((serviceProvider, options) =>
+{
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+    var connectionString = configuration.GetConnectionString("DefaultConnection");
+    options.UseNpgsql(connectionString);
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

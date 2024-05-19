4929c2bf00ad288b3e3957875b1f62636f3ccbd6
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-05-19 22:47:34 +0300

    SignalRApi project added

---
+++ Traversal/SignalRApi/DAL/Visitor.cs
@@ -0,0 +1,18 @@
+﻿namespace SignalRApi.DAL
+{
+    public enum ECity
+    {
+        Diyarbakır = 1,
+        İstanbul = 2,
+        Ankara = 3,
+        Erzurum = 4,
+        Edirne = 5,
+    }
+    public class Visitor
+    {
+        public int VisitorID { get; set; }
+        public ECity City { get; set; }
+        public int CityVisitorCount { get; set; }
+        public DateTime VisitorDate { get; set; }
+    }
+}
